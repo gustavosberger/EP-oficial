369fc82833aa5bf2ca184e9972ac46435d679d33
mikomoares <mihanoch@gmail.com>
2018-04-16 14:45:29 -0300

    progesso no py sem berguer

---
+++ item01(progesso).py
@@ -0,0 +1,34 @@
+#estoque da loja
+estoque={}
+
+#definindo escolha do menu
+escolha=1
+
+#enquanto não escolherem 0, o programa fica repetindo
+while escolha!=0:
+    escolha=int(input("faça sua escolha:"))
+    
+#opção de menu 1
+    if escolha==1:
+        produto=input("nome do produto:")
+        while produto in estoque:
+            print ("este produto já existe")
+            produto=input("nome do produto:")
+        quantidade_inicial=int(input("quantidade:"))
+        while quantidade_inicial<0:
+            print ("a quantidade inicial não pode ser negativa")
+            quantidade_inicial=int(input("quantidade:"))
+        caracteristica={"quantidade":quantidade_inicial}
+        estoque[produto]=caracteristica
+        print ("{0} {1}s foram adicionadas".format(quantidade_inicial, produto))
+        
+#opção de menu 2  
+    elif escolha == 3:
+        produto=input('digite o nome do produto - ')
+        if produto in estoque:
+            valor_adicional = int(input('quantidade do produto - '))
+            estoque[produto]['quantidade'] += valor_adicional
+            print ('novo estoque de {0} é {1}'.format(produto,estoque[produto]['quantidade']))
+        else:
+            print ('elemento não encontrado')
+        
