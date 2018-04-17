93e0cff7b84c6d5626862d708208bb9c0126767f
gustavosberger <gustavosberger@gmail.com>
2018-04-17 09:28:32 -0300

    tentei fazer esse item, vê se ta certo

---
+++ item2(tentativa-bergao).py
@@ -0,0 +1,6 @@
+import.json
+with open ("item01(progesso).py","r") as tentativa
+    conteudo= json.load(tentativa)
+with open ("item01(progesso).py","w") as tentativa2
+    conteudo2= json.dumps(tentativa2)
+    
