e6a4c643127f5a0c974de7fda6f845a9aa75be2f
roguetaver <andretavernaro@gmail.com>
2018-04-17 21:55:36 -0300

    corrigido o erro do item03

--- item03.py
+++ item03.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+Ôªø# -*- coding: utf-8 -*-
 """
 Created on Tue Apr 17 15:48:58 2018
 
 @author: mihan
 """
 
-#menuzinho das opÁıes
+#menuzinho das op√ß√µes
 print("0- sair")
 print("1-adicionar produto")
 print("2-remover produto")
@@ -19,61 +19,61 @@ estoque={}
 #definindo escolha do menu
 escolha=1
 
-#enquanto n„o escolherem 0, o programa fica repetindo
+#enquanto n√£o escolherem 0, o programa fica repetindo
 while escolha!="0":
-    escolha=input("faÁa sua escolha:")
+    escolha=input("fa√ßa sua escolha:")
     
-#opÁ„o de menu 1
+#op√ß√£o de menu 1
     if escolha=="1":
         produto=input("nome do produto:")
         while produto in estoque:
-            print ("este produto j· existe")
+            print ("este produto j√° existe")
             produto=input("nome do produto:")
         quantidade_inicial=int(input("quantidade:"))
-        preco = float(input('valor unitario:'))
+        preco= float(input('valor unitario:'))
         while quantidade_inicial<0:
-            print ("a quantidade inicial n„o pode ser negativa")
+            print ("a quantidade inicial n√£o pode ser negativa")
             quantidade_inicial=int(input("quantidade:"))
             preco = float(input('valor unitario:'))
         caracteristica={"quantidade":quantidade_inicial,'valor unitario':preco}
         estoque[produto]=caracteristica
         print ("{0} {1}s foram adicionadas, custando {2} reais cada".format(quantidade_inicial, produto,preco))
 
-#opÁ„o de menu 2
+#op√ß√£o de menu 2
     elif escolha == "2":
         remover= input("Digite o nome do produto que deseja remover: ")
         while remover not in estoque:
-            print ("Produto n„o encontrado")
-            remover = input ("Digite um produto v·lido: ")
+            print ("Produto n√£o encontrado")
+            remover = input ("Digite um produto v√°lido: ")
         if remover in estoque:
             del estoque [remover]
             print ("{0} foi removido".format(remover))
         
-#opÁ„o de menu 3
+#op√ß√£o de menu 3
     elif escolha == "3":
         produto=input('digite o nome do produto: ')
         while produto not in estoque:
-            print ('elemento n„o encontrado')
+            print ('elemento n√£o encontrado')
             produto= input ("digite o nome do produto: ")
-        opcao_mudanca= input("1-para mudar preÁo. 2-para mudar quantidade: ")
+        opcao_mudanca= input("Para mudar o pre√ßo digite: 1 ou Para mudar a quantidade digite: 2 - ")
         while opcao_mudanca!="1" and opcao_mudanca!="2":
-            print("comando inv·lido")
-            opcao_mudanca= input("1-para mudar preÁo. 2-para mudar quantidade")
+            print("comando inv√°lido")
+            opcao_mudanca= input("Para mudar o pre√ßo digite: 1 ou Para mudar a quantidade digite: 2")
         if opcao_mudanca=="1":
-            alteracao_preco= float(input('novo valor do produto: ')
-            estoque[produto]["valor unitario"] = alteracao_preco
+            alteracao_preco= float(input('novo valor do produto: '))
+            estoque[produto]['valor unitario'] = alteracao_preco
         elif opcao_mudanca=="2":
             valor_adicional = int(input('quantidade do produto: '))
             estoque[produto]['quantidade'] += valor_adicional
-        print ('novo estoque de {0} È {1}, e seu novo preÁo È de {2}'.format(produto,estoque[produto]['quantidade'],estoque[produto]['valor unitario']))
+        print ('novo estoque de {0} √© {1}, e seu novo pre√ßo √© de {2}'.format(produto,estoque[produto]['quantidade'],estoque[produto]['valor unitario']))
         
 
-#opÁ„o de menu 4
+#op√ß√£o de menu 4
     elif escolha == "4":
         print("Estoque:")
         for chave, valor in estoque.items():
             print ("{0} : {1} , {2}".format(chave,valor["quantidade"], valor ['valor unitario']))
     elif escolha != "0":
-        print ("Comando inv·lido")
+        print ("Comando inv√°lido")
 
-print ("AtÈ a prÛxima, amigo!")
+print ("At√© a pr√≥xima, amigo!")
