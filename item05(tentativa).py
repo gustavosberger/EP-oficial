79f27ddbc824dd5d15cd3f7cb0f07a76795c9b11
roguetaver <andretavernaro@gmail.com>
2018-04-19 22:44:20 -0300

    tentativa do item 05

--- item05(tentativa).py
+++ item05(tentativa).py
@@ -1,3 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 22:04:13 2018
+
+@author: andre
+"""
+
 import json 
 
 with open ('estoque.json','r') as entrada:
@@ -27,6 +34,10 @@ while escolha!="0":
 #opção de menu 1
     if escolha=="1":
         print ("para cancelar digite: 0")
+        loja = input("digite o nome da loja")
+        while loja not in estoque:
+            print('essa loja não existe')
+            loja = input("digite o nome da loja")
         produto=input("nome do produto:")
         while produto in estoque and produto!="0":
             print ("este produto já existe")
@@ -39,7 +50,7 @@ while escolha!="0":
                 quantidade_inicial=int(input("quantidade: "))
                 preco = float(input('valor unitario: '))
             caracteristica={"quantidade":quantidade_inicial,'valor unitario':preco}
-            estoque[produto]=caracteristica
+            estoque[loja][produto]=caracteristica
             #menuzinho:
             print("0- sair")
             print("1-adicionar produto")
@@ -56,8 +67,12 @@ while escolha!="0":
 #opção de menu 2
     elif escolha == "2":
         print ("para cancelar digite: 0")
+        loja = input("digite o nome da loja")
+        while loja not in estoque:
+            print('essa loja não existe')
+            loja = input("digite o nome da loja")
         remover= input("Digite o nome do produto que deseja remover: ")
-        while remover not in estoque and remover!="0":
+        while remover not in estoque[loja] and remover!="0":
             print ("Produto não encontrado")
             remover = input ("Digite um produto válido: ")
         print("0- sair")
@@ -68,8 +83,8 @@ while escolha!="0":
         print("5-Produtos que estão em falta")
         print("6-valor monetário total do estoque")
         if remover !="0":
-            if remover in estoque:
-                del estoque [remover]
+            if remover in estoque[loja]:
+                del estoque[loja][remover]
                 print ("{0} foi removido".format(remover))
         else:
             print ("operação cancelada")
@@ -80,8 +95,12 @@ while escolha!="0":
 #opção de menu 3
     elif escolha == "3":
         print ("para cancelar digite: 0")
+        loja = input("digite o nome da loja")
+        while loja not in estoque:
+            print('essa loja não existe')
+            loja = input("digite o nome da loja")
         produto=input('digite o nome do produto: ')
-        while produto not in estoque and produto!="0":
+        while produto not in estoque[loja] and produto!="0":
             print ('elemento não encontrado')
             produto= input ("digite o nome do produto: ")
         if produto !="0":
@@ -94,10 +113,10 @@ while escolha!="0":
                 while alteracao_preco<0:
                     print ("não é possível preço negativo")
                     alteracao_preco= float(input('novo valor do produto: '))
-                estoque[produto]['valor unitario'] = alteracao_preco
+                estoque[loja][produto]['valor unitario'] = alteracao_preco
             elif opcao_mudanca=="2":
                 valor_adicional = int(input('quantidade do produto: '))
-                estoque[produto]['quantidade'] += valor_adicional
+                estoque[loja][produto]['quantidade'] += valor_adicional
             print("0- sair")
             print("1-adicionar produto")
             print("2-remover produto")
@@ -105,7 +124,7 @@ while escolha!="0":
             print("4-mostrar estoque completo")
             print("5-Produtos que estão em falta")
             print("6-valor monetário total do estoque")
-            print ('novo estoque de {0} é {1}, e seu novo preço é de R${2}'.format(produto,estoque[produto]['quantidade'],estoque[produto]['valor unitario']))
+            print ('novo estoque de {0} é {1}, e seu novo preço é de R${2}'.format(produto,estoque[loja][produto]['quantidade'],estoque[loja][produto]['valor unitario']))
         else:
             print("0- sair")
             print("1-adicionar produto")
@@ -129,7 +148,8 @@ while escolha!="0":
         print("5-Produtos que estão em falta")
         print("6-valor monetário total do estoque")
         print("Estoque:")
-        for chave, valor in estoque.items():
+        loja = input('digite o nome da loja')
+        for chave, valor in estoque[loja][produto].items():
             print ("{0} : {1} , R${2}".format(chave,valor["quantidade"], valor ['valor unitario']))
 
 #opção de menu 5
@@ -142,7 +162,8 @@ while escolha!="0":
         print("5-Produtos que estão em falta")
         print("6-valor monetário total do estoque")
         print("Produtos que estão em falta no estoque:")
-        for chave,valor in estoque.items():
+        loja = input('digite o nome da loja')
+        for chave,valor in estoque[loja][produto].items():
             if valor["quantidade"] < 0 :
                 print (chave)   
 
@@ -157,7 +178,8 @@ while escolha!="0":
         print("5-Produtos que estão em falta")
         print("6-valor monetário total do estoque")
         print("valor monetário total do estoque:")
-        for valor in estoque.values():
+        loja=input('digite o nome da loja')
+        for valor in estoque[loja][produto].values():
             if valor["quantidade"] <= 0:
                 listinha.append(0)
             elif valor["quantidade"] > 0:
